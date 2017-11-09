0f61e0f4a98bc3317dd22e699a2fae6c8d4b37fc
rdogmartin <rogerweb@techinfosystems.com>
2017-11-09 16:03:39 -0700

    Added jQuery

--- Gs.WebClient/gs-app/src/app/app.component.ts
+++ Gs.WebClient/gs-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    $(document).ready(() => {
+      if ($) {
+          console.log('jQuery is loaded and available.');
+      } else {
+          console.log('jQuery is NOT loaded.');
+      }
+  });
+  }
   title = 'Gallery Server';
 }
